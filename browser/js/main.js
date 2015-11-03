151464b0aceb09224229e3fbe8e4c6ac4ea28de4
Mutahhir <mutahhir.ali@gmail.com>
2015-11-03 08:07:19 +0500

    Add some custom styling to CodeMirror

--- browser/js/main.js
+++ browser/js/main.js
@@ -2,6 +2,8 @@ import 'codemirror/mode/markdown/markdown';
 import 'codemirror/lib/codemirror.css!';
 import CodeMirror from 'codemirror';
 
+import '../css/styles.css!';
+
 
 CodeMirror.fromTextArea(document.querySelector('#editor'), {
   mode: 'markdown',
