27bf3ee47a0c928da867b4df700cce4933882adc
Mutahhir <mutahhir.ali@gmail.com>
2015-11-04 05:30:50 +0500

    Use IPC to open file

--- browser/js/main.js
+++ browser/js/main.js
@@ -4,8 +4,15 @@ import CodeMirror from 'codemirror';
 
 import '../css/styles.css!';
 
+const IPC = System._nodeRequire('ipc');
+import events from '../../events';
 
-CodeMirror.fromTextArea(document.querySelector('#editor'), {
-  mode: 'markdown',
-  lineWrapping: true
+const codeMirror =
+  CodeMirror.fromTextArea(document.querySelector('#editor'), {
+    mode: 'markdown',
+    lineWrapping: true
+  });
+
+IPC.on(events.openfile, function (fileContents) {
+  codeMirror.setValue(fileContents);
 });
